166a2c4adcd68d6a875099192bc2b18e638bb5fa
Kaveesha Weerasiri <weerasirikaveesha@gmail.com>
2021-06-04 17:47:35 -0500

    might have fixed the server issue

--- waitingScreen.js
+++ waitingScreen.js
@@ -1,4 +1,5 @@
 let waitingText = document.querySelector("#waitingText");
+let startGameButton = document.querySelector("#start-game");
 let socket = io();
 
 socket.on("waiting", function (data) {
@@ -10,24 +11,27 @@ socket.on("waiting", function (data) {
     "The number of players left to start the game is: " +
     numberPlayersNeededToPlay;
 
-  //start game - required number of players is met  
-  // if (numberPlayersNeededToPlay == 0) {
-  //   socket.emit("startGame");
-  //   //startGameButton.disabled = false;
-  // }
+  //start game - required number of players is met
+  if (numberPlayersNeededToPlay == 0) {
+    startGameButton.disabled = false;
+  }
 });
 
-socket.on("playerLeft", function(data) {
-  //update the screen to display number of clients need to start a game
-  console.log("in client side");
-  console.log(data);
-  let numberPlayersNeededToPlay = data;
-  waitingText.innerHTML =
-    "The number of players left to start the game is: " +
-    numberPlayersNeededToPlay;
+startGameButton.addEventListener("click", () => {
+  location.href = "./clientBoard.html";
 });
 
-//client has recieved message to redirct to the board
-socket.on("startGame", () => {
-  window.location = "http://localhost:8080/clientBoard.html";
-});
+// socket.on("playerLeft", function(data) {
+//   //update the screen to display number of clients need to start a game
+//   console.log("in client side");
+//   console.log(data);
+//   let numberPlayersNeededToPlay = data;
+//   waitingText.innerHTML =
+//     "The number of players left to start the game is: " +
+//     numberPlayersNeededToPlay;
+// });
+
+// //client has recieved message to redirct to the board
+// socket.on("startGame", () => {
+//   window.location = "http://localhost:8080/clientBoard.html";
+// });
